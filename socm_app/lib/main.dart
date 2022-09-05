a10e99910e446c76f1a6019b141dfa1363a1a19c
mukesh9739 <mukesh9739@gmail.com>
2022-09-05 15:55:05 +0200

    Add files via upload

---
+++ socm_app/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:socm_app/widgets/mqttView.dart';
+import 'package:socm_app/mqtt/state/MQTTAppState.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: ChangeNotifierProvider<MQTTAppState>(
+          create: (_) => MQTTAppState(),
+          child: MQTTView(),
+        ));
+  }
+}
